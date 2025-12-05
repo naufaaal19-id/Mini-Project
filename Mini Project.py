80a57556bc5484a1afd59a61ffa3b0962ab6a60c
mooonroe <maimunah542313@gmail.com>
2025-12-05 22:47:10 +0700

    commit #2

--- Mini Project.py
+++ Mini Project.py
@@ -18,3 +18,27 @@ def lihat_barang(inventaris):
         print("\nDaftar Barang:")
         for i, barang in enumerate(inventaris, start=1):
             print(f"{i}. {barang['nama']} - Jumlah: {barang['jumlah']} - Harga: {barang['harga']}")
+
+# Fungsi edit barang
+def edit_barang(inventaris):
+    lihat_barang(inventaris)
+    if inventaris:
+        nomor = int(input("Nomor barang: ")) - 1
+        if 0 <= nomor < len(inventaris):
+            inventaris[nomor]['nama'] = input("Nama baru: ")
+            inventaris[nomor]['jumlah'] = int(input("Jumlah baru: "))
+            inventaris[nomor]['harga'] = int(input("Harga baru: "))
+            print("Barang berhasil diedit!")
+        else:
+            print("Nomor tidak valid.")
+
+# Fungsi hapus barang
+def hapus_barang(inventaris):
+    lihat_barang(inventaris)
+    if inventaris:
+        nomor = int(input("Nomor barang: ")) - 1
+        if 0 <= nomor < len(inventaris):
+            barang = inventaris.pop(nomor)
+            print(f"Barang '{barang['nama']}' dihapus!")
+        else:
+            print("Nomor tidak valid.")
